958fc88e93f2e1a974b96dc524ed6d980a6d7149
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-05-16 22:34:12 -0300

    update composite

--- PadroesDeProjetos/Composite/src/composite/Main.java
+++ PadroesDeProjetos/Composite/src/composite/Main.java
@@ -3,11 +3,9 @@ package composite;
 public class Main {
 	   public static void main(String[] args) {
 	        Pikachu pikachu = new Pikachu();
-	        Charmander charmander = new Charmander();
 
 	        EquipePokemon equipe = new EquipePokemon();
 	        equipe.adicionar(pikachu);
-	        equipe.adicionar(charmander);
 
 	        equipe.mostrar();
 	    }
